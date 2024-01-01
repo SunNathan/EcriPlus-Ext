8dd1e12b4ad93777ac26a3964570935f75e8cfdc
Nathan Sun <nathan.sunyt@gmail.com>
2024-01-01 13:11:06 +0100

    fix: remove logs

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 let explication = "Explication indisponible. Merci de rafraichir la page.";
 function catchAssessment(details) {
     if (details.url.startsWith("https://api.tests.ecriplus.fr/api/assessments/") && details.url.endsWith("/next") && details.originUrl.startsWith("https://app.tests.ecriplus.fr/assessments")) {
+
         // console.log(details)
         // console.log(details.requestHeaders)
         let authHeader = details.requestHeaders.find(header => header.name.toLowerCase() === "authorization");
@@ -13,33 +14,28 @@ function catchAssessment(details) {
                 ...details.requestHeaders,
                 'Authorization': `Bearer ${myToken}`
             },
-            mode: 'same-origin', // Utilisez 'same-origin' ou 'cors' selon vos besoins
+            mode: 'same-origin',
             redirect: 'follow'
         })
-            .then(response => response.json())  // Supposons que la réponse est au format JSON
+            .then(response => response.json())
             .then(jsonData => {
-                // console.log(jsonData.data.attributes.explication)
                 explication = filterProblematicCharacters(jsonData.data.attributes.explication);
+                // console.log(tabid)
                 printExplication(explication,tabid);
             })
             .catch(error => console.error("Erreur lors de la récupération de la nouvelle réponse :", error));
     }
-
-    // Retourner les détails de la requête inchangés
     return { requestHeaders: details.requestHeaders };
 }
 function filterProblematicCharacters(text) {
-    // Remplacer les guillemets simples par une séquence d'échappement
     text = text.replace(/'/g, "\\'");
-
-    // Supprimer les occurrences de &nbsp;
     text = text.replace(/&nbsp;/g, " ");
 
     return text;
 }
 
 function printExplication(explication, tabId) {
-
+        // console.log("avant ajout", explication);
         setTimeout(() => {
             browser.tabs.executeScript(tabId, {
                 code: `
@@ -48,7 +44,7 @@ function printExplication(explication, tabId) {
                 if (!instructionSection) console.log('errreur');`
             });
             // console.log('ajouté', explication);
-        }, 1500); // Délai en millisecondes (2 secondes = 2000millisecondes)
+        }, 1000); // Délai en millisecondes (2 secondes = 2000millisecondes)
 
 }
 
